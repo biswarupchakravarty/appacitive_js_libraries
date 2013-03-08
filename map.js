96fb70145169d9f1821b1c02f5b80821d963ba28
biswarupchakravarty <bchakravarty@appacitive.com>
2013-03-08 14:37:26 +0530

    changed map.

--- map.js
+++ map.js
@@ -1,10 +1,12 @@
 {
-	"hub": {
-		"angrybirds": {
-			"handler1": "h1"
-		},
-		"sandbox": {
-			"handler2": "h2"
-		}
+	"angry_birds_handler": {
+		"accountName": "hub",
+		"applicationName": "angrybirds",
+		"environment": "live"
+	},
+	"invent_handler": {
+		"accountName": "hub",
+		"applicationName": "invent",
+		"environment": "sandbox"
 	}
 }
